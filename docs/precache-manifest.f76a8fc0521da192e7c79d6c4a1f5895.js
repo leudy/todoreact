ed95b7bb7fdc0ad5a92199ef0e4826c247c2eeed
Leudi Rosario <leudi_rosario@hotmail.com>
2020-07-03 16:14:05 -0400

    rebuild and deploy the app

---
+++ docs/precache-manifest.f76a8fc0521da192e7c79d6c4a1f5895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba4eb36d755ef6ebe36919ac9e945d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247aa3c0e67cd976ab84",
+    "url": "/static/css/main.771c0741.chunk.css"
+  },
+  {
+    "revision": "8ee47b64357ad6691701",
+    "url": "/static/js/2.0a41a697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a41a697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247aa3c0e67cd976ab84",
+    "url": "/static/js/main.f517a6af.chunk.js"
+  },
+  {
+    "revision": "df9beda1d3c1c8a489a1",
+    "url": "/static/js/runtime-main.a382552b.js"
+  }
+]);
